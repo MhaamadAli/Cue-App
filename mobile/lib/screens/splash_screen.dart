0fa815be5a2a3c75b6c1a48724fe70ddf03d8b8d
MhaamadAli <m.alialaawar@gmail.com>
2024-04-27 18:05:48 +0300

    SplashSCreen: added custom font and styling

--- mobile/lib/screens/splash_screen.dart
+++ mobile/lib/screens/splash_screen.dart
@@ -8,8 +8,15 @@ class SplashScreen extends StatelessWidget {
     return const Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Text('CUE')
+          child: Text(
+        'CUE',
+        style: TextStyle(
+          fontSize: 64,
+          color: Color(0xFF06D6A0),
+          fontFamily: 'Lexend',
+          fontWeight: FontWeight.w900
         ),
+      ),),
     );
   }
 }
