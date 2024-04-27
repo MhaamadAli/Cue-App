11141fd59d1a6c11ab6bb88a9ac1d04df1947cd1
MhaamadAli <m.alialaawar@gmail.com>
2024-04-27 21:07:09 +0300

    OnboardingScreen: added appBar with skip button

--- mobile/lib/screens/onboarding_screen.dart
+++ mobile/lib/screens/onboarding_screen.dart
@@ -5,6 +5,31 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    // Skip button logic
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
