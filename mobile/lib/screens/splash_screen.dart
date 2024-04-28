34f589027704f9703d26d01dc08d6f23772853dc
MhaamadAli <m.alialaawar@gmail.com>
2024-04-28 17:23:53 +0300

    increased timer duration

--- mobile/lib/screens/splash_screen.dart
+++ mobile/lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     // Start a timer that redirects to another page after 2 seconds
-    Timer(Duration(seconds: 1), () {
+    Timer(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacementNamed("/onboarding");
     });
   }
