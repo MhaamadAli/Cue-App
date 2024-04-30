46822b5ba99fc9d2939ae93ebb9889ae3eecaabf
MhaamadAli <m.alialaawar@gmail.com>
2024-04-30 04:49:46 +0300

    finished Greeting section

--- mobile/lib/screens/Home/home.dart
+++ mobile/lib/screens/Home/home.dart
@@ -5,18 +5,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Row(
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 backgroundImage: AssetImage('assets/profileAvatar.png'),
-                radius: 30,
+                radius: 25,
               ),
-              SizedBox(width: 10),
-              
+              const SizedBox(width: 10),
+              const Expanded(
+                child: Text(
+                  'Hello Mohammad!',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF07AB81),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.notifications),
+                  iconSize: 25,
+                  color: Colors.white,
+                  onPressed: () {
+                    print('notification clicked');
+                  },
+                ),
+              )
             ],
           ),
         ),
