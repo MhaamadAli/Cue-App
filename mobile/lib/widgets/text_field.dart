f18b2a0c3579b96aba71aa416680fac5785033c3
MhaamadAli <m.alialaawar@gmail.com>
2024-04-30 13:02:50 +0300

    fixed padding for textField widget

--- mobile/lib/widgets/text_field.dart
+++ mobile/lib/widgets/text_field.dart
@@ -16,7 +16,7 @@ class MyTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      padding: const EdgeInsets.symmetric(horizontal: 0.0),
       child: TextField(
         controller: controller,
         obscureText: obscureText,
