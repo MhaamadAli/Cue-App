126e44961f62ad1f54be41a8e6a15b68a6609486
MhaamadAli <m.alialaawar@gmail.com>
2024-04-30 04:33:53 +0300

    created the profile avatar

--- mobile/lib/screens/Home/home.dart
+++ mobile/lib/screens/Home/home.dart
@@ -5,6 +5,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/profileAvatar.png'),
+                radius: 30,
+              ),
+              SizedBox(width: 10),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
