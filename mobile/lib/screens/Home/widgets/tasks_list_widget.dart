f0517f52cbb0762ae9fd3c83301cdb15e72d2fa8
MhaamadAli <m.alialaawar@gmail.com>
2024-05-01 13:19:14 +0300

    created TasksListWidget

---
+++ mobile/lib/screens/Home/widgets/tasks_list_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TasksListWidget extends StatelessWidget {
+  const TasksListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
