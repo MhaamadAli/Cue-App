c4a57b7dec59f8b84cebd5e48f2bfce08be0ad04
MhaamadAli <m.alialaawar@gmail.com>
2024-05-01 09:02:44 +0300

    changed HomeScreen to a statefulwidget to implement bottom navigation

--- mobile/lib/screens/Home/home.dart
+++ mobile/lib/screens/Home/home.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/screens/Assistant/assistant_screen.dart';
 import 'package:mobile/screens/Home/widgets/meeting_list_widget.dart';
+import 'package:mobile/screens/Tasks/tasks_screen.dart';
+import 'package:mobile/screens/map/map_screen.dart';
+import 'package:mobile/screens/schedule/schedule_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    // home
+    HomeScreen(),
+    // tasks
+    TasksScreen(),
+    // assistant
+    AssistantScreen(),
+    // schedule
+    ScheduleScreen(),
+    // map
+    MapScreen()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -53,9 +83,7 @@ class HomeScreen extends StatelessWidget {
                     MeetingListCard(),
                     SizedBox(width: 20),
                     MeetingListCard(),
-                    SizedBox(
-                      width: 20
-                    ),
+                    SizedBox(width: 20),
                     MeetingListCard(),
                   ],
                 ),
