825c94c41e3098f7ebc2e80fd78edc9d3e7ff59f
MhaamadAli <m.alialaawar@gmail.com>
2024-05-01 12:37:24 +0300

    added routes for new screens and fixed main theme font

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/screens/Assistant/assistant_screen.dart';
+
 import 'package:mobile/screens/Home/home.dart';
+import 'package:mobile/screens/Tasks/tasks_screen.dart';
 import 'package:mobile/screens/auth/login_screen.dart';
 import 'package:mobile/screens/auth/signup_screen.dart';
+import 'package:mobile/screens/map/map_screen.dart';
 import 'package:mobile/screens/onboarding/welcome_screen.dart';
 import 'package:mobile/screens/onboarding/onboarding_screen.dart';
+import 'package:mobile/screens/schedule/schedule_screen.dart';
 import 'package:mobile/screens/splash_screen.dart';
 
 void main() {
@@ -16,21 +22,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CUE personal buddy',
-      theme: ThemeData(
-        fontFamily: 'Lexend',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-        useMaterial3: true,
-      ),
-      routes: {
-        "/":(context) => SplashScreen(),
-        "/onboarding":(context) => OnboardingScreen(),
-        "/welcome":(context) => WelcomeScreen(),
-        "/login":(context) => LoginScreen(),
-        "/signup":(context) => SignupScreen(),
-        "/home":(context) => HomeScreen(),
-      }
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'CUE personal buddy',
+        theme: ThemeData(
+          primarySwatch: Colors.lightGreen,
+          textTheme: TextTheme(
+            headlineLarge:
+                GoogleFonts.lexendDeca(fontSize: 24, fontWeight: FontWeight.bold),
+            bodyMedium: GoogleFonts.roboto(fontSize: 18,fontWeight: FontWeight.w300),
+          ),
+          useMaterial3: true,
+        ),
+        routes: {
+          "/": (context) => SplashScreen(),
+          "/onboarding": (context) => OnboardingScreen(),
+          "/welcome": (context) => WelcomeScreen(),
+          "/login": (context) => LoginScreen(),
+          "/signup": (context) => SignupScreen(),
+          "/home": (context) => HomeScreen(),
+          "/schedule": (context) => ScheduleScreen(),
+          "/tasks":(context) => TasksScreen(),
+          "/map":(context) => MapScreen(),
+          "/assistant":(context) => AssistantScreen()
+
+        });
   }
 }
