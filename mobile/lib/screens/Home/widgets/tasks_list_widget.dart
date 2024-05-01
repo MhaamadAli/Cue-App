8fbcaa58b9ce771c7b36e985a419321a945c5e5e
MhaamadAli <m.alialaawar@gmail.com>
2024-05-01 16:20:40 +0300

    created background container and added styling

--- mobile/lib/screens/Home/widgets/tasks_list_widget.dart
+++ mobile/lib/screens/Home/widgets/tasks_list_widget.dart
@@ -5,6 +5,23 @@ class TasksListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: 380,
+      height: 450,
+      margin: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFF07AB81).withOpacity(0.4),
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFF07AB81).withOpacity(0.3),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+    );
   }
 }
