ceb503ccb3e3112160b3c92e141ab5c728f56dee
MhaamadAli <m.alialaawar@gmail.com>
2024-05-01 11:38:13 +0300

    created Assistant screen

---
+++ mobile/lib/screens/Assistant/assistant_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AssistantScreen extends StatelessWidget {
+  const AssistantScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
