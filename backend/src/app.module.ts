fffee0921bc07c977d341a330f06e800dcec3958
MhaamadAli <m.alialaawar@gmail.com>
2024-05-04 21:58:20 +0300

    backend: created the schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { MeetingsModule } from './meetings/meetings.module';
 import { TasksModule } from './tasks/tasks.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { FeedbacksModule } from './feedbacks/feedbacks.module';
 
 @Module({
-  imports: [UsersModule, MeetingsModule, TasksModule, NotificationsModule],
+  imports: [UsersModule, MeetingsModule, TasksModule, NotificationsModule, FeedbacksModule],
   controllers: [AppController],
   providers: [AppService],
 })
