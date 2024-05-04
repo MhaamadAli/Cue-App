c14bb493b51f2e018351b20564422f72677a2ca5
MhaamadAli <m.alialaawar@gmail.com>
2024-05-04 21:26:20 +0300

    backend: created modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MeetingsModule } from './meetings/meetings.module';
+import { TasksModule } from './tasks/tasks.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MeetingsModule, TasksModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
