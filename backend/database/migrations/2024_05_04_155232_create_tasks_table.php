1b95e74620637002eec262aeaddf1e2141cff3b3
MhaamadAli <m.alialaawar@gmail.com>
2024-05-04 19:00:46 +0300

    backend: created create_tasks_migration

---
+++ backend/database/migrations/2024_05_04_155232_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['todo', 'done'])->default('todo');
+            $table->dateTime('due_datetime')->nullable();
+            $table->foreignId('location_tags')->nullable()->constrained('locations')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
