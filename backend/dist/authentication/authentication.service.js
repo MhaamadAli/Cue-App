c7da4ef97664a151eb3a1e65d5902350e332fbd2
MhaamadAli <m.alialaawar@gmail.com>
2024-05-05 11:05:18 +0300

    Revert merge of 'nest'

---
+++ backend/dist/authentication/authentication.service.js
@@ -0,0 +1,35 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthenticationService = void 0;
+const common_1 = require("@nestjs/common");
+const passport_1 = require("@nestjs/passport");
+const passport_auth0_1 = require("passport-auth0");
+let AuthenticationService = class AuthenticationService extends (0, passport_1.PassportStrategy)(passport_auth0_1.Strategy, 'auth0') {
+    constructor() {
+        super({
+            domain: process.env.AUTH0_DOMAIN,
+            clientID: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            callbackURL: process.env.AUTH0_CALLBACK_URL,
+            state: false,
+        });
+    }
+    validate(accessToken, refreshToken, extraParams, profile, done) {
+        done(null, profile);
+    }
+};
+exports.AuthenticationService = AuthenticationService;
+exports.AuthenticationService = AuthenticationService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [])
+], AuthenticationService);
+//# sourceMappingURL=authentication.service.js.map
