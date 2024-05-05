6ab57dec7cd2533aea7d73aff36906a314889f85
MhaamadAli <m.alialaawar@gmail.com>
2024-05-05 10:37:04 +0300

    backend: generated authentication module and service, modified the authentication service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { MeetingsModule } from './meetings/meetings.module';
 import { TasksModule } from './tasks/tasks.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { FeedbacksModule } from './feedbacks/feedbacks.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [UsersModule, MeetingsModule, TasksModule, NotificationsModule, FeedbacksModule],
+  imports: [UsersModule, MeetingsModule, TasksModule, NotificationsModule, FeedbacksModule, AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
