9511d1070ef480a636f26862a041dfb0f42e20b0
MhaamadAli <m.alialaawar@gmail.com>
2024-05-05 11:06:58 +0300

    backend: resolving conflict

--- backend/dist/authentication/authentication.service.d.ts
+++
@@ -1,6 +0,0 @@
-declare const AuthenticationService_base: any;
-export declare class AuthenticationService extends AuthenticationService_base {
-    constructor();
-    validate(accessToken: string, refreshToken: string, extraParams: any, profile: any, done: (err: any, user?: any, info?: any) => void): void;
-}
-export {};
