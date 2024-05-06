d52389ab20f84184f0ee02e36634397395b3cbdb
MhaamadAli <m.alialaawar@gmail.com>
2024-05-06 15:12:33 +0300

    backend: finished meetings CRUD

---
+++ backend/src/meetings/meetings.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MeetingsService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(createMeetingDto: Prisma.MeetingCreateInput) {
+    return this.prismaService.meeting.create({
+      data: createMeetingDto,
+    });
+  }
+
+  async findAll() {
+    return this.prismaService.meeting.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.prismaService.meeting.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateMeetingDto: Prisma.MeetingUpdateInput) {
+    return this.prismaService.meeting.update({
+      where: {
+        id,
+      },
+      data: updateMeetingDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.meeting.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
