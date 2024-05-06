286e39c3668f877453ff82a825cb0fbda2d4a50f
MhaamadAli <m.alialaawar@gmail.com>
2024-05-06 11:55:40 +0300

    backend: created CRUD for Tasks

---
+++ backend/src/tasks/tasks.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(createTaskDto: Prisma.TaskCreateInput) {
+    return this.prismaService.task.create({
+      data: createTaskDto,
+    });
+  }
+
+  async findAll() {
+    return this.prismaService.task.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.prismaService.task.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateTaskDto: Prisma.TaskUpdateInput) {
+    return this.prismaService.task.update({
+      where: {
+        id,
+      },
+      data: updateTaskDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.task.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
