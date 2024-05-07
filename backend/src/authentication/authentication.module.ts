90c982170c383f775ca469fa3f86cb95b339870e
MhaamadAli <m.alialaawar@gmail.com>
2024-05-07 14:19:37 +0300

    backend: updated authentication.module.ts

--- backend/src/authentication/authentication.module.ts
+++ backend/src/authentication/authentication.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { AuthenticationService } from './authentication.service';
+import { AuthService } from './authentication.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PassportModule],
-  providers: [AuthenticationService],
-  exports: [AuthenticationService],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'your-secret-whatever',
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
-export class AuthenticationModule {}
+export class AuthModule {}
