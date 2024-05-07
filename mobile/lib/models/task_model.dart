ebffe437bba6c70d254f1ab83889d488af77e113
MhaamadAli <m.alialaawar@gmail.com>
2024-05-07 21:37:29 +0300

    frontend: updated Task Model

--- mobile/lib/models/task_model.dart
+++ mobile/lib/models/task_model.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
+
 class Task {
   final int id;
   final String title;
   final String description;
+  final Color bannerColor;
+  final bool isChecked;
 
-  Task({required this.id, required this.title, required this.description});
+  Task({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.isChecked = false,
+    required this.bannerColor,
+  });
 }
