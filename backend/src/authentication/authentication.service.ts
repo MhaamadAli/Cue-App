08dae019a0daca8fb48f7c3a43aee7f2c88e407b
MhaamadAli <m.alialaawar@gmail.com>
2024-05-07 14:17:09 +0300

    backend: updated ValidateUser method

--- backend/src/authentication/authentication.service.ts
+++ backend/src/authentication/authentication.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-auth0';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class AuthenticationService extends PassportStrategy(Strategy, 'auth0') {
-  constructor() {
-    super({
-      domain: process.env.AUTH0_DOMAIN,
-      clientID: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: process.env.AUTH0_CALLBACK_URL,
-      state: false,
-    });
-  }
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private prismaService: PrismaService,
+  ) {}
 
-  validate(
-    accessToken: string,
-    refreshToken: string,
-    extraParams: any,
-    profile: any,
-    done: (err: any, user?: any, info?: any) => void,
-  ): void {
-    done(null, profile);
+  async validateUser(email: string, password: string): Promise<any> {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { email },
+      });
+      if (user && (await bcrypt.compare(password, user.password_hash))) {
+        const { password_hash, ...result } = user;
+        return result;
+      }
+      return null;
+    } catch (error) {
+      console.error('Error validating user:', error);
+      return null;
+    }
   }
 }
