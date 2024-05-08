a2bddfe677b956fa87bcc54eb590bb543b4b9b55
MhaamadAli <m.alialaawar@gmail.com>
2024-05-08 13:16:10 +0300

    frontend: Implemented the schedule screen with custom calendar

--- mobile/lib/screens/schedule/schedule_screen.dart
+++ mobile/lib/screens/schedule/schedule_screen.dart
@@ -1,10 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/models/meeting_model.dart';
+import 'package:mobile/screens/schedule/widgets/meeting_card.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class ScheduleScreen extends StatelessWidget {
-  const ScheduleScreen({super.key});
+class ScheduleScreen extends StatefulWidget {
+  @override
+  _ScheduleScreenState createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  DateTime _selectedDay = DateTime.now();
+
+  final List<Meeting> _meetings = [
+    Meeting(
+      id: 1,
+      title: 'Finish UI - Nour',
+      description: 'You should finish the UI with Flutter before Saturday',
+      duedatetime: DateTime.now(),
+    ),
+    Meeting(
+      id: 2,
+      title: 'Flutter - Taha',
+      description: 'Meeting with Taha to have a code review',
+      duedatetime: DateTime.now(),
+    ),
+    Meeting(
+      id: 3,
+      title: 'Backend - Chris',
+      description: 'Meeting with Chris to review backend code',
+      duedatetime: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Color(0xFF07AB81)),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/home');
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add, color: Color(0xFF07AB81)),
+            onPressed: () {
+              // Implement your action for adding new events
+            },
+          ),
+        ],
+        title: const Text('Schedule', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: <Widget>[
+            TableCalendar(
+              focusedDay: _selectedDay,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              calendarFormat: CalendarFormat.week,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                });
+              },
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+                titleTextStyle: const TextStyle(color: Colors.white),
+                leftChevronIcon:
+                    const Icon(Icons.chevron_left, color: Colors.white),
+                rightChevronIcon:
+                    const Icon(Icons.chevron_right, color: Colors.white),
+                decoration: BoxDecoration(
+                  color: Color(0xFF07AB81),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              calendarStyle: const CalendarStyle(
+                defaultTextStyle: TextStyle(color: Colors.black54),
+                weekendTextStyle: TextStyle(color: Colors.black54),
+                todayDecoration: BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            MeetingCard(inputMeeting: _meetings[1])
+          ],
+        ),
+      ),
+    );
   }
 }
