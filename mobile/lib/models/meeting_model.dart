e8b3d0b006f05b44af0c316d89e8f55aad83d5a5
MhaamadAli <m.alialaawar@gmail.com>
2024-05-08 09:58:47 +0300

    frontend: updated Meeting model and implemented formattedTime methods

--- mobile/lib/models/meeting_model.dart
+++ mobile/lib/models/meeting_model.dart
@@ -1,7 +1,22 @@
+import 'package:intl/intl.dart';
+
 class Meeting {
   final int id;
   final String title;
   final String description;
+  final DateTime duedatetime;
 
-  Meeting({required this.id, required this.title, required this.description});
+  Meeting(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.duedatetime});
+
+  String getFormattedDate() {
+    return DateFormat('yyyy-MM-dd').format(duedatetime);
+  }
+
+  String getFormattedTime() {
+    return DateFormat('HH:mm').format(duedatetime);
+  }
 }
