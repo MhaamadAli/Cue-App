dbcc585b79e2b24c68e515856b4aaf35e2d69b0a
MhaamadAli <m.alialaawar@gmail.com>
2024-05-08 18:59:09 +0300

    frontend: implemented addMarker functionality

--- mobile/lib/screens/map/map_screen.dart
+++ mobile/lib/screens/map/map_screen.dart
@@ -9,7 +9,20 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final MapController _mapController = MapController();
+  List<Marker> markers = [];
   LatLng? _currentPosition;
+
+  void _addMarker(LatLng pos) {
+    final marker = Marker(
+      width: 50.0,
+      height: 50.0,
+      point: pos,
+      child: const Icon(Icons.location_pin, color: Colors.red, size: 30),
+    );
+    setState(() {
+      markers.add(marker);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +32,14 @@ class _MapScreenState extends State<MapScreen> {
         options: MapOptions(
           initialCenter: _currentPosition ?? const LatLng(45.5231, -122.6765),
           initialZoom: 13.0,
+          onTap: (_, pos) => _addMarker(pos),
         ),
         children: [
           TileLayer(
             urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             subdomains: const ['a', 'b', 'c'],
           ),
+          MarkerLayer(markers: markers),
         ],
       ),
     );
