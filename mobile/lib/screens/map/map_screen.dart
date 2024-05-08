1580585711fb844f8bb4e7b783d0f173d80c85ec
MhaamadAli <m.alialaawar@gmail.com>
2024-05-08 18:58:24 +0300

    frontend: added mapController and basic map view

--- mobile/lib/screens/map/map_screen.dart
+++ mobile/lib/screens/map/map_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapScreen extends StatelessWidget {
-  const MapScreen({super.key});
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final MapController _mapController = MapController();
+  LatLng? _currentPosition;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FlutterMap(
+        mapController: _mapController,
+        options: MapOptions(
+          initialCenter: _currentPosition ?? const LatLng(45.5231, -122.6765),
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+          ),
+        ],
+      ),
+    );
   }
 }
