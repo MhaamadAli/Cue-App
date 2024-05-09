b8744195e5e274a64236c4cfca91dc7b8540f7f2
MhaamadAli <m.alialaawar@gmail.com>
2024-05-09 09:43:57 +0300

    frontend: added methods for dealing with JWT token

--- mobile/lib/utilities/secure_storage.dart
+++ mobile/lib/utilities/secure_storage.dart
@@ -3,4 +3,15 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SecureStorage {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
+  Future<void> storeToken(String token) async {
+    await _storage.write(key: 'jwtToken', value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'jwtToken');
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'jwtToken');
+  }
 }
