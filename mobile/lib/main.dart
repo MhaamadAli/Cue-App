4df564eb9882b73db301cf0806d353045e7e2ae9
MhaamadAli <m.alialaawar@gmail.com>
2024-05-09 09:28:02 +0300

    Configured  main.dart to use dotenv and initialize UserProvider

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,9 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart'; // Import provider package
 import 'package:mobile/screens/Assistant/assistant_screen.dart';
-
 import 'package:mobile/screens/Home/home.dart';
 import 'package:mobile/screens/Tasks/tasks_screen.dart';
 import 'package:mobile/screens/auth/login_screen.dart';
@@ -12,8 +14,10 @@ import 'package:mobile/screens/onboarding/onboarding_screen.dart';
 import 'package:mobile/screens/profile/profile_screen.dart';
 import 'package:mobile/screens/schedule/schedule_screen.dart';
 import 'package:mobile/screens/splash_screen.dart';
+import 'package:mobile/provider/user_provider.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -22,15 +26,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'CUE personal buddy',
         theme: ThemeData(
           primarySwatch: Colors.lightGreen,
           textTheme: TextTheme(
-            headlineLarge:
-                GoogleFonts.lexendDeca(fontSize: 24, fontWeight: FontWeight.bold),
-            bodyMedium: GoogleFonts.roboto(fontSize: 18,fontWeight: FontWeight.w300),
+            headlineLarge: GoogleFonts.lexendDeca(
+                fontSize: 24, fontWeight: FontWeight.bold),
+            bodyMedium:
+                GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.w300),
           ),
           useMaterial3: true,
         ),
@@ -42,10 +49,12 @@ class MyApp extends StatelessWidget {
           "/signup": (context) => SignupScreen(),
           "/home": (context) => HomeScreen(),
           "/schedule": (context) => ScheduleScreen(),
-          "/tasks":(context) => TasksScreen(),
-          "/map":(context) => MapScreen(),
-          "/assistant":(context) => AssistantScreen(),
-          "/profile":(context) => ProfileScreen()
-        });
+          "/tasks": (context) => TasksScreen(),
+          "/map": (context) => MapScreen(),
+          "/assistant": (context) => AssistantScreen(),
+          "/profile": (context) => ProfileScreen()
+        },
+      ),
+    );
   }
 }
