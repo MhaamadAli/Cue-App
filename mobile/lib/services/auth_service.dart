db38e0a8ad3b96ea28c44bedb66225bcf666c014
MhaamadAli <m.alialaawar@gmail.com>
2024-05-09 09:40:45 +0300

    frontend: added login method logic

--- mobile/lib/services/auth_service.dart
+++ mobile/lib/services/auth_service.dart
@@ -6,6 +6,15 @@ class AUTHService {
   final baseUrl = dotenv.env['API_URL'];
 
   Future<Map<String, dynamic>> login(String email, String password) async {
-    
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    var responseData = json.decode(response.body);
+    return responseData;
   }
 }
