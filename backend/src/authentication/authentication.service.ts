7cf3f45a7a5a3b2eaa69a942029eb7b40bbae396
MhaamadAli <m.alialaawar@gmail.com>
2024-05-09 06:39:07 +0300

    backend: modified auth.service login

--- backend/src/authentication/authentication.service.ts
+++ backend/src/authentication/authentication.service.ts
@@ -43,8 +43,10 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
+    const access_token = this.jwtService.sign(payload);
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: access_token,
+      user: user,
     };
   }
 }
