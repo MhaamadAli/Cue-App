83ea77974d94bde5105ffbb85ab66cc7cb6b02e4
MhaamadAli <m.alialaawar@gmail.com>
2024-05-09 06:39:38 +0300

    backend: enabled CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-
-
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
