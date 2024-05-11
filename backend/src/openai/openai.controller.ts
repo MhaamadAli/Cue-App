5447e5f5b23ad67573cb4b7d1a1d42c049c176df
MhaamadAli <m.alialaawar@gmail.com>
2024-05-11 14:41:55 +0300

    backend: implemented chat endpoint to handle post requests

--- backend/src/openai/openai.controller.ts
+++ backend/src/openai/openai.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, HttpException, HttpStatus } from '@nestjs/common';
 import { ChatGptService } from './openai.service';
+import { Post, Body } from '@nestjs/common';
+import { Message } from './dto/message.dto';
 
 @Controller('openai')
 export class OpenAIController {
   constructor(private readonly chatGptService: ChatGptService) {}
+
+  @Post('chat')
+  async getChatResponse(@Body() body: { prompt: string; messages: Message[] }) {
+    const response = await this.chatGptService.chatGptRequest(
+      body.prompt,
+      body.messages,
+    );
+    return { response };
+  }
 }
