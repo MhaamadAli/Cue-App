c4fa816f83be3e3fe10fa308e9ec5211eb378ee4
MhaamadAli <m.alialaawar@gmail.com>
2024-05-11 22:04:21 +0300

    backend: applied migrations

---
+++ backend/prisma/migrations/20240511182907_updated_meeting/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reminder_time` on the `Meeting` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Meeting" DROP COLUMN "reminder_time";
