d7c7a5836ad621435c1bf42b35264b783f05d655
MhaamadAli <m.alialaawar@gmail.com>
2024-05-11 14:32:35 +0300

    backend: implemented task creation handling in ChatGPT service

--- backend/src/openai/openai.service.ts
+++ backend/src/openai/openai.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import OpenAIApi from 'openai';
+import { ChatCompletion, ChatCompletionMessageParam } from 'openai/resources';
+import { Message } from './dto/message.dto';
 
 @Injectable()
 export class ChatGptService {
@@ -9,5 +11,53 @@ export class ChatGptService {
     this.openai = new OpenAIApi({
       apiKey: process.env.OPEN_AI_SECRET_KEY,
     });
+  }
+
+  async chatGptRequest(prompt: string, messages: Message[]): Promise<any> {
+    try {
+      const history = messages.map(
+        (message): ChatCompletionMessageParam => ({
+          role: message.ai ? 'assistant' : 'user',
+          content: message.text,
+        }),
+      );
+
+      const completion: ChatCompletion =
+        await this.openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: [
+            {
+              role: 'system',
+              content: prompt,
+            },
+            ...history,
+          ],
+          temperature: 0.5,
+          max_tokens: 1000,
+        });
+
+      const choice = completion.choices[0];
+      if (!choice.message) {
+        return { error: 'No response from GPT-3' };
+      }
+      const content = choice.message.content;
+
+      if (
+        content.toLowerCase().includes('reminder') ||
+        content.toLowerCase().includes('task')
+      ) {
+        return {
+          type: 'task',
+          object: {
+            title: prompt,
+            description: content,
+          },
+        };
+      }
+      return { error: 'Response not relevant to tasks' };
+    } catch (e) {
+      console.error(e);
+      throw new ServiceUnavailableException('Failed request to ChatGPT');
+    }
   }
 }
