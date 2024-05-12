805965c3067a7af12842f98ccfcdd8175374651d
MhaamadAli <m.alialaawar@gmail.com>
2024-05-12 12:02:11 +0300

    frontend: created fetchAllTasks

--- mobile/lib/services/tasks_service.dart
+++ mobile/lib/services/tasks_service.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:mobile/models/task_model.dart';
+import 'package:mobile/utilities/secure_storage.dart';
+
 class TaskService {
+  final String baseUrl = 'http://localhost:3000';
+  final SecureStorage _secureStorage = SecureStorage();
+
+  TaskService();
+
+  Future<List<Task>> fetchAllTasks() async {
+    final authToken = await _secureStorage.getToken();
+    final response = await http.get(
+      Uri.parse('$baseUrl/tasks/'),
+    );
+    List<dynamic> tasksJson = json.decode(response.body);
+    return tasksJson.map((json) => Task.fromJson(json)).toList();
+  }
   
 }
