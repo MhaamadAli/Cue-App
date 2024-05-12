429b198de47183bb835e7c8137b6200b65ee7989
MhaamadAli <m.alialaawar@gmail.com>
2024-05-12 18:35:04 +0300

    frontend: created meeting_service

---
+++ mobile/lib/services/meeting_service.dart
@@ -0,0 +1,9 @@
+import 'package:mobile/utilities/secure_storage.dart';
+
+class MeetingService {
+  final String baseUrl = 'http://localhost:3000';
+  final SecureStorage secureStorage;
+
+  MeetingService(this.secureStorage);
+
+}
