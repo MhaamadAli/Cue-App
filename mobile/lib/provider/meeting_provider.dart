d9a9c42dec540d3e5dfa384ef18b7d1cd7d32d15
MhaamadAli <m.alialaawar@gmail.com>
2024-05-12 18:42:11 +0300

    frontend: implemented meting provider

--- mobile/lib/provider/meeting_provider.dart
+++ mobile/lib/provider/meeting_provider.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/models/meeting_model.dart';
 import 'package:mobile/services/meeting_service.dart';
 
 class MeetingsProvider with ChangeNotifier {
   final MeetingService meetingService;
+  List<Meeting> _meetings = [];
 
   MeetingsProvider(this.meetingService);
+
+  List<Meeting> get meetings => _meetings;
+
+  Future<void> loadMeetings() async {
+    _meetings = await meetingService.fetchMeetings();
+    notifyListeners();
+  }
 }
