f084e76b3f7ffc78cb4185e54cf6d12a6c8270a8
MhaamadAli <m.alialaawar@gmail.com>
2024-05-12 12:13:22 +0300

    frontend: added TasksService and added alltasks

--- mobile/lib/provider/task_provider.dart
+++ mobile/lib/provider/task_provider.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/models/task_model.dart';
+import 'package:mobile/services/tasks_service.dart';
 
 class TasksProvider with ChangeNotifier {
+  List<Task> _allTasks = [];
+  final TaskService _taskService;
 
+  TasksProvider(this._taskService);
+
+  List<Task> get allTasks => _allTasks;
 }
