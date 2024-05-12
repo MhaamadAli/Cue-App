23958e0e1ebd276e5707436ce5d0bd685cf49a1d
MhaamadAli <m.alialaawar@gmail.com>
2024-05-12 18:37:29 +0300

    frontend: implemented fetchMeetings

--- mobile/lib/services/meeting_service.dart
+++ mobile/lib/services/meeting_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:mobile/models/meeting_model.dart';
 import 'package:mobile/utilities/secure_storage.dart';
 
@@ -7,5 +9,14 @@ class MeetingService {
 
   MeetingService(this.secureStorage);
 
-  Future<List<Meeting>> fetchMeetings() async {}
+  Future<List<Meeting>> fetchMeetings() async {
+    final authToken = await secureStorage.getToken();
+    final response = await http.get(
+      Uri.parse('$baseUrl/meetings'),
+      headers: {'Authorization': 'Bearer $authToken'},
+    );
+
+    List<dynamic> meetingsJson = json.decode(response.body);
+    return meetingsJson.map((json) => Meeting.fromJson(json)).toList();
+  }
 }
