68e128b0d4766960c9b060bd4fbc17b9f826fe01
MhaamadAli <m.alialaawar@gmail.com>
2024-05-14 09:01:22 +0300

    frontend: initialized cue dashboard

---
+++ cue_dashboard/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cue_dashboard/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CUE Dashboard',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        "/": (context) => SplashScreen(),
+      },
+    );
+  }
+}
