b00b362cc6b2ccb2cd3c1fbb2f95272f663d55dc
MhaamadAli <m.alialaawar@gmail.com>
2024-05-14 18:20:00 +0300

    frontend: added final adjustements for textField

--- cue_dashboard/lib/widgets/my_text_field.dart
+++ cue_dashboard/lib/widgets/my_text_field.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyTextField extends StatelessWidget {
+  final dynamic controller;
+  final String hintText;
+  final bool obscureText;
+
   const MyTextField({
     super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
   });
 
   @override
@@ -10,18 +18,20 @@ class MyTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 0.0),
       child: TextField(
-        obscureText: false,
+        controller: controller,
+        obscureText: obscureText,
         decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Color(0xFFBCB7B7)),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey.shade400),
-          ),
-          fillColor: const Color.fromARGB(255, 255, 255, 255),
-          filled: true,
-        ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Color(0xFFBCB7B7)),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: const Color.fromARGB(255, 255, 255, 255),
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
       ),
     );
   }
