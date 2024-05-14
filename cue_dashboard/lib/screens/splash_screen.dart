4e3c39c61bdd104c2183e4f35cfee00b1b59a5c1
MhaamadAli <m.alialaawar@gmail.com>
2024-05-14 10:29:32 +0300

    frontend: implemented splash screen for dashboard

--- cue_dashboard/lib/screens/splash_screen.dart
+++ cue_dashboard/lib/screens/splash_screen.dart
@@ -1,10 +1,49 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).popAndPushNamed("/login");
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'C U E',
+              style: GoogleFonts.lexendDeca(
+                fontSize: 66,
+                color: Color(0xFF06D6A0),
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            Text(
+              'Your personal Buddy',
+              style: GoogleFonts.roboto(
+                fontSize: 24,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
