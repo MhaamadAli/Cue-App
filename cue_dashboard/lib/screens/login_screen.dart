947d4bd3dcbeb863b92593ad240c191f4093b255
MhaamadAli <m.alialaawar@gmail.com>
2024-05-14 19:00:02 +0300

    frontend: modified basic structure and styling for login

--- cue_dashboard/lib/screens/login_screen.dart
+++ cue_dashboard/lib/screens/login_screen.dart
@@ -1,38 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/logo.png',
-                width: 350,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Welcome Back',
-                    style: GoogleFonts.monomaniacOne(
-                        fontSize: 44, fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(width: 10),
-                  Image.asset(
-                    'assets/hand.png',
-                    width: 50,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 800, maxHeight: 650),
+            child: Container(
+              padding: const EdgeInsets.all(40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF06D6A0).withOpacity(0.2),
+                    spreadRadius: 15,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
                   ),
                 ],
               ),
-            ],
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [],
+              ),
+            ),
           ),
         ),
       ),
