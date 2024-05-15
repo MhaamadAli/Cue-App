5ee1b98cf56152291a21db5ea69f191ea3a4fad0
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 11:01:18 +0300

    frontend: added dashboard route

--- cue_dashboard/lib/main.dart
+++ cue_dashboard/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cue_dashboard/screens/dashboard/dashboard_screen.dart';
 import 'package:cue_dashboard/screens/login_screen.dart';
 import 'package:cue_dashboard/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,12 +16,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'CUE Dashboard',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
       routes: {
         "/": (context) => SplashScreen(),
-        "/login": (context) => LoginScreen(), 
+        "/login": (context) => LoginScreen(),
+        "/dashboard": (context) => DashboardPage(),
       },
     );
   }
