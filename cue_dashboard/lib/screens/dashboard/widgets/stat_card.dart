b163b4684a11f2f0a6384f8735f1f566c83807d5
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 11:18:53 +0300

    frontend: created initial structure for stat_card

---
+++ cue_dashboard/lib/screens/dashboard/widgets/stat_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class StatCard extends StatelessWidget {
+  final String title;
+  final String count;
+
+  StatCard({required this.title, required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(16.0),
+        margin: const EdgeInsets.all(8.0),
+      ),
+    );
+  }
+}
