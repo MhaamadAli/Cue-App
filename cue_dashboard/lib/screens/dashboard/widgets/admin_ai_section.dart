bed069783f111c37c58913afbbe7c2363fffef05
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 11:34:23 +0300

    frontend: created admin ai section

---
+++ cue_dashboard/lib/screens/dashboard/widgets/admin_ai_section.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdminAISection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.all(8.0),
+    );
+  }
+}
