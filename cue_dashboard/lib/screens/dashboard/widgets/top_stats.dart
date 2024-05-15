2e041b105eca3d4c1cc686f0ef2272e9911aa6ec
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 11:17:40 +0300

    frontend: implemented top_stats widget with statCard

--- cue_dashboard/lib/screens/dashboard/widgets/top_stats.dart
+++ cue_dashboard/lib/screens/dashboard/widgets/top_stats.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'stat_card.dart';
 
+class TopStats extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        StatCard(
+          title: 'Number of Users',
+          count: '29',
+        ),
+        StatCard(
+          title: 'New Users This Month',
+          count: '12',
+        ),
+      ],
+    );
+  }
+}
