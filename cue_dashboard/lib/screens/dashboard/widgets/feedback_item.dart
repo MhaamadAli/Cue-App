0a779204f3d3351216d09d7ac15312669a2aa3d6
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 11:05:26 +0300

    frontend: initialized feedbackitem

---
+++ cue_dashboard/lib/screens/dashboard/widgets/feedback_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FeedbackItem extends StatelessWidget {
+  final String date;
+  final String userName;
+  final String feedback;
+  final Color indicatorColor;
+
+  FeedbackItem({
+    required this.date,
+    required this.userName,
+    required this.feedback,
+    required this.indicatorColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
