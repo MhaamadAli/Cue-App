3beee855f4266f454646d93deb486436e1b1a49d
MhaamadAli <m.alialaawar@gmail.com>
2024-05-15 17:35:32 +0300

    frontend: created AiService

---
+++ mobile/lib/services/ai_service.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:mobile/utilities/secure_storage.dart';
+
+class AiService {
+  final String baseUrl = 'http://localhost:3000';
+}
