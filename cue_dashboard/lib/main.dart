6cf9353dfaaedb05f294beb06b904989fe46ef42
MhaamadAli <m.alialaawar@gmail.com>
2024-05-16 10:02:47 +0300

    frontend: added providers

--- cue_dashboard/lib/main.dart
+++ cue_dashboard/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:cue_dashboard/provider/feedback_provider.dart';
+import 'package:cue_dashboard/provider/user_provider.dart';
 import 'package:cue_dashboard/screens/dashboard/dashboard_screen.dart';
 import 'package:cue_dashboard/screens/login_screen.dart';
 import 'package:cue_dashboard/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +15,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CUE Dashboard',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      routes: {
-        "/": (context) => SplashScreen(),
-        "/login": (context) => LoginScreen(),
-        "/dashboard": (context) => DashboardPage(),
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => UserProvider()),
+          ChangeNotifierProvider(create: (context) => FeedbackProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'CUE Dashboard',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            useMaterial3: true,
+          ),
+          routes: {
+            "/": (context) => SplashScreen(),
+            "/login": (context) => LoginScreen(),
+            "/dashboard": (context) => DashboardPage(),
+          },
+        ));
   }
 }
