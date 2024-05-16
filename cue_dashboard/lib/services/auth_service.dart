d477a54bf8235246b393c49a70f7eff7ade11b4b
MhaamadAli <m.alialaawar@gmail.com>
2024-05-16 13:31:09 +0300

    frontend: implemented login in auth_service

--- cue_dashboard/lib/services/auth_service.dart
+++ cue_dashboard/lib/services/auth_service.dart
@@ -3,4 +3,19 @@ import 'package:http/http.dart' as http;
 
 class AUTHService {
   final String baseUrl = 'http://192.168.101.207:3000';
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+      }),
+    );
+    var responseData = json.decode(response.body);
+    return responseData;
+  }
 }
