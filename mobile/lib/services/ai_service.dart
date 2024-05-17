df92f078a70a1ffb3d7961f0c28d698e92c249e9
MhaamadAli <m.alialaawar@gmail.com>
2024-05-17 22:45:58 +0300

    frontend: modified ai_service

--- mobile/lib/services/ai_service.dart
+++ mobile/lib/services/ai_service.dart
@@ -8,9 +8,6 @@ class AiService {
   Future<void> sendAiMessage(String messageText) async {
     final url = Uri.parse('$baseUrl/openai/chat');
     final token = await SecureStorage().getToken();
-    const prompt =
-        "act like a personal assistant that is responsible for createing tasks and scheduling meeting, return responsee in parsable json format. Basically i will be requestion to create one of two things, task or meeting. when i say something like 'create a task to remind me to buy groceries tomorrow' you will return a json object that has a type having a value 'task', 'object' having the value as { 'title': 'buy groceries', 'description': don't forget to buy groceries from the groceries store'} while when i say something like 'schedule a meeting with chris for tomorrow at 3pm' you wil return a json object that has a type of meeting a title 'meeting with chris' and a description: 'you have a meeting with chris at 3pm' and a date and time";
-
     final response = await http.post(
       url,
       headers: {
@@ -18,7 +15,6 @@ class AiService {
         'Authorization': 'Bearer $token',
       },
       body: jsonEncode({
-        'prompt': prompt,
         'messages': [
           {'text': messageText, 'ai': false},
         ]
