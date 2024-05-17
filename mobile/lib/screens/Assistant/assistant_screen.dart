04fa2be9a369ab77c8ba942cb460953e568cc66d
MhaamadAli <m.alialaawar@gmail.com>
2024-05-17 22:44:48 +0300

    frontend: added assistant screen animation

--- mobile/lib/screens/Assistant/assistant_screen.dart
+++ mobile/lib/screens/Assistant/assistant_screen.dart
@@ -3,78 +3,110 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile/services/ai_service.dart';
 
 class AssistantScreen extends StatefulWidget {
-  const AssistantScreen({super.key});
+  final VoidCallback onBack;
+
+  const AssistantScreen({super.key, required this.onBack});
 
   @override
   _AssistantScreenState createState() => _AssistantScreenState();
 }
 
-class _AssistantScreenState extends State<AssistantScreen> {
+class _AssistantScreenState extends State<AssistantScreen>
+    with SingleTickerProviderStateMixin {
   final TextEditingController _textController = TextEditingController();
   final AiService _aiService = AiService();
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFD3E0CA),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Center(
-              child: Container(
-                height: 300,
-                width: 300,
-                child: ClipOval(
-                  child: Image.asset(
-                    'assets/assistantAnimation.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
+      backgroundColor: const Color(0xFFD3E0CA),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: <Widget>[
                 IconButton(
-                  icon: const Icon(Icons.mic, color: Colors.green),
-                  onPressed: () {
-                    // Handle microphone press
-                    print('Microphone pressed');
-                  },
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: _textController,
-                    decoration: InputDecoration(
-                      hintText: 'Press the mic and start speaking or type',
-                      hintStyle: GoogleFonts.lato(
-                        color: Colors.grey,
-                        fontSize: 16,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.send, color: Colors.green),
-                        onPressed: _handleSend,
+                  onPressed: widget.onBack,
+                  icon: const Icon(Icons.arrow_back),
+                )
+              ],
+            ),
+            Expanded(
+              child: Center(
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  child: RotationTransition(
+                    turns: _animation,
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/assistantAnimation.png',
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.mic, color: Colors.green),
+                    onPressed: _handleSend,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'Press the mic and start speaking or type',
+                        hintStyle: GoogleFonts.lato(
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.send, color: Colors.green),
+                          onPressed: _handleSend,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
