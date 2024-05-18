4d1794c3333d133757828088ed72c6877f9ac1fe
MhaamadAli <m.alialaawar@gmail.com>
2024-05-18 11:10:48 +0300

    backend: added findAll test

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../authentication/auth.guard';
+import { Role } from '@prisma/client';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -34,5 +35,22 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const result = [
+        {
+          id: 1,
+          username: 'test',
+          email: 'test@example.com',
+          password_hash: 'hashedpassword',
+          role: Role.USER,
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
   });
 });
