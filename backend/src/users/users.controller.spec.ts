982a2e5cab5f821aa124a0b2d8d200f2f17f91b6
MhaamadAli <m.alialaawar@gmail.com>
2024-05-18 11:15:49 +0300

    backend: added update and delete test

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../authentication/auth.guard';
-import { Role } from '@prisma/client';
+import { Role, Prisma } from '@prisma/client';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -69,6 +69,37 @@ describe('UsersController', () => {
       jest.spyOn(service, 'findOne').mockResolvedValue(result);
 
       expect(await controller.findOne('1')).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const updateUserDto: Prisma.UserUpdateInput = { username: 'updated' };
+      const result = {
+        id: 1,
+        username: 'updated',
+        email: 'test@example.com',
+        password_hash: 'hashedpassword',
+        role: Role.USER,
+      };
+      jest.spyOn(service, 'update').mockResolvedValue(result);
+
+      expect(await controller.update('1', updateUserDto)).toBe(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user', async () => {
+      const result = {
+        id: 1,
+        username: 'test',
+        email: 'test@example.com',
+        password_hash: 'hashedpassword',
+        role: Role.USER,
+      };
+      jest.spyOn(service, 'remove').mockResolvedValue(result);
+
+      expect(await controller.remove('1')).toBe(result);
     });
   });
 });
