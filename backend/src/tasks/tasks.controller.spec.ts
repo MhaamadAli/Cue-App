5a9df7c043f5e6a61263c152b0e26c2cbae11c70
MhaamadAli <m.alialaawar@gmail.com>
2024-05-19 23:45:25 +0300

    backend: initialized tasks.controller tests

---
+++ backend/src/tasks/tasks.controller.spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { JwtAuthGuard } from '../authentication/auth.guard';
+
+describe('TasksController', () => {
+  let controller: TasksController;
+  let service: TasksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TasksController],
+      providers: [
+        {
+          provide: TasksService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            getNotDone: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: jest.fn(() => true),
+      })
+      .compile();
+
+    controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
