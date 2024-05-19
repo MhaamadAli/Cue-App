7f4a7328a4aec3784a17e1f2b67e5d85b945e0b3
MhaamadAli <m.alialaawar@gmail.com>
2024-05-19 23:51:00 +0300

    backend: initialized tasks.service test

---
+++ backend/src/tasks/tasks.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('TasksService', () => {
+  let service: TasksService;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        {
+          provide: PrismaService,
+          useValue: {
+            task: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<TasksService>(TasksService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
